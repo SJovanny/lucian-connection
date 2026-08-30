082bb5459c56c5372c980c9b15bd8b3e79eaf8b1
SJovanny <142829604+SJovanny@users.noreply.github.com>
2026-08-31 00:33:42 +0200

    feat: add .env.example file for Supabase configuration and update .gitignore to include it

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,4 @@
 import { createBrowserClient } from "@supabase/ssr";
-<<<<<<< HEAD
-=======
-import type { SupabaseClient } from "@supabase/supabase-js";
->>>>>>> 8fe42b97f85853eb971c445806efb905789fcda1
 import type { Database } from "@/types/database.types";
 
 export function createClient() {
